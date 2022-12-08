5e401be3aa9b0638e9bb80d45b5ec0964e1e9465
Gustavo Delbon Souza <61765239+MrDelbon@users.noreply.github.com>
2022-12-08 19:08:39 -0300

    Commit Final

--- js/script.js
+++ js/script.js
@@ -16,12 +16,12 @@ var swiper = new Swiper(".home-slider", {
    loop:true,
    centeredSlides:true,
    navigation: {
-     nextEl: ".swiper-button-next",
-     prevEl: ".swiper-button-prev",
+   nextEl: ".swiper-button-next",
+   prevEl: ".swiper-button-prev",
    },
 });
 
-var swiper = new Swiper(".food-slider", {
+var swiper = new Swiper(".learning-slider", {
    grabCursor:true,
    loop:true,
    centeredSlides:true,
@@ -32,24 +32,27 @@ var swiper = new Swiper(".food-slider", {
    },
    breakpoints: {
       0: {
-        slidesPerView: 1,
+      slidesPerView: 1,
       },
       700: {
-        slidesPerView: 2,
+      slidesPerView: 2,
       },
       1000: {
-        slidesPerView: 3,
+      slidesPerView: 3,
       },
    },
 });
 
-let previewContainer = document.querySelector('.food-preview-container');
-let previewBox = previewContainer.querySelectorAll('.food-preview');
+let previewContainer = document.querySelector('.learning-preview-container');
+let previewBox = previewContainer.querySelectorAll('.learning-preview');
+var video1 = document.getElementById('video1');
+var video2 = document.getElementById('video3');
+var video3 = document.getElementById('video2');
 
-document.querySelectorAll('.food .slide').forEach(food =>{
-   food.onclick = () =>{
+document.querySelectorAll('.learning .slide').forEach(learning =>{
+   learning.onclick = () =>{
       previewContainer.style.display = 'flex';
-      let name = food.getAttribute('data-name');
+      let name = learning.getAttribute('data-name');
       previewBox.forEach(preveiw =>{
          let target = preveiw.getAttribute('data-target');
          if(name == target){
@@ -63,6 +66,12 @@ previewContainer.querySelector('#close-preview').onclick = () =>{
    previewContainer.style.display = 'none';
    previewBox.forEach(close =>{
       close.classList.remove('active');
+      video1.pause();
+      video2.pause();
+      video3.pause();
+      video1.currentTime = 0;
+      video2.currentTime = 0;
+      video3.currentTime = 0;
    });
 };
 
@@ -78,13 +87,13 @@ var swiper = new Swiper(".blogs-slider", {
    },
    breakpoints: {
       0: {
-        slidesPerView: 1,
+      slidesPerView: 1,
       },
       700: {
-        slidesPerView: 2,
+      slidesPerView: 2,
       },
       1000: {
-        slidesPerView: 3,
+      slidesPerView: 3,
       },
    },
 });
